70378b0c469abbb49c20a828e5f0c1f05857dedb
tonico4 <antogarcia.personal@gmail.com>
2023-03-05 10:05:55 +0100

    Work experience and education form

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -6,8 +6,8 @@ const Generator = () => {
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [location, setLocation] = useState('');
-    const [education, setEducation] = useState([]);
-    const [workExperience, setWorkExperience] = useState([]);
+    const [education, setEducation] = useState([{school: '', graduationYear: ''}]);
+    const [workExperience, setWorkExperience] = useState([{company: '', position: '', description: '', startDate: '', endDate: ''}]);
     const [history, setHistory] = useState('');
     const [titles, setTitles] = useState('');
     const [skills, setSkills] = useState('');
@@ -69,7 +69,34 @@ const Generator = () => {
         <div>
             <h1>Create your CV:</h1>
             <form>
-
+                <h2>Educación</h2>
+                {education.map((edu, index) => (
+                    <div key={index}>
+                        <label>Escuela:</label>
+                        <input type="text" name="school" value={edu.school} onChange={(e) => handleEducationChange(e, index)}></input>
+                        <label>Año de graduación:</label>
+                        <input type="text" name="graduationYear" value={edu.graduationYear} onChange={(e) => handleEducationChange(e, index)}></input>
+                        {index > 0 && <button type="button" onClick={() => handleRemoveEducation(index)}>Borrar</button>}
+                    </div>
+                ))}
+                <button type="button" onClick={() => handleAddEducation}>Añadir</button>
+                <h2>Experiencia laboral</h2>
+                {workExperience.map((work, index) => (
+                    <div key={index}>
+                        <label>Empresa:</label>
+                        <input type="text" name="company" value={work.company} onChange={(e) => handleWorkExperienceChange(e, index)}></input>
+                        <label>Cargo:</label>
+                        <input type="text" name="position" value={work.position} onChange={(e) => handleWorkExperienceChange(e, index)}></input>
+                        <label>Descripción:</label>
+                        <input type="text" name="description" value={work.description} onChange={(e) => handleWorkExperienceChange(e, index)}></input>
+                        <label>Año de inicio:</label>
+                        <input type="text" name="startDate" value={work.startDate} onChange={(e) => handleWorkExperienceChange(e, index)}></input>
+                        <label>Año de fin:</label>
+                        <input type="text" name="endDate" value={work.endDate} onChange={(e) => handleWorkExperienceChange(e, index)}></input>
+                        {index > 0 && <button type="button" onClick={() => handleRemoveWorkExperience(index)}>Borrar</button>}
+                    </div>
+                ))}
+                <button type="button" onClick={() => handleAddWorkExperience}>Añadir</button>
             </form>
         </div>
     );
